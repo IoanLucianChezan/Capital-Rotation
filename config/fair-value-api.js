f810e3b778f0ecdaef42ada7cb222f90420d97a6
IoanLucianChezan <lucian.chezan@nttdata.com>
2026-09-24 13:28:38 +0300

    chore: remove fair value feature

--- config/fair-value-api.js
+++
@@ -1,3 +0,0 @@
-// După deploy-ul Cloudflare Worker, înlocuiește valoarea cu URL-ul workers.dev.
-// Exemplu: "https://capital-rotation-fair-value.numele-tau.workers.dev"
-window.FAIR_VALUE_API_URL = "";
